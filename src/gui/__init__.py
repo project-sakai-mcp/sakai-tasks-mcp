0e57cf480e42b4849d1a6b891d1486533190ab16
sayt25606-rgb <sayt25606@gmail.com>
2026-09-26 21:26:20 +0900

    feat(gui): implement src/gui/__init__.py (#78)

---
+++ src/gui/__init__.py
@@ -0,0 +1,11 @@
+"""GUI package for user settings and initial setup."""
+
+from src.gui.settings_window import (
+    show_initial_setup_window,
+    show_settings_window,
+)
+
+__all__ = [
+    "show_settings_window",
+    "show_initial_setup_window",
+]
